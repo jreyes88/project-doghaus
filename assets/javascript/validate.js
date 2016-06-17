075ec46dcd417c03a221f62c9cfc7665633c5346
Michael <Multishifties.multishifties@yahoo.com>
2016-06-17 18:32:38 -0500

    Verification modals working, unstyled

--- assets/javascript/validate.js
+++ assets/javascript/validate.js
@@ -1,5 +1,6 @@
 $('document').ready(function(){
-
+	//hide popups
+	$(".popupDiv").hide();
 	//name of submit button
 	$(".sendThisAlready").on("click",function(){
 		console.log("click!!");
@@ -19,9 +20,33 @@ $('document').ready(function(){
 		if(customerEmail.indexOf("@")== -1 || customerEmail.indexOf(".")==- 1){
 			responseMessage = "Please enter a valid e-mail address";
 			console.log(responseMessage);	
+			$(".popupDivEmail").html(responseMessage);
+			$(".popupDivEmail").show(600, function(){
+				timeoutID = window.setTimeout(hideDiv, 2000);
+				function hideDiv(){
+					$(".popupDivEmail").hide(600);
+				};
+			});
 		}else if(num1.length<3 || num2.length<3 || num3.length<4){
 			responseMessage = "Please complete your phone number";
 			console.log(responseMessage);
+			$(".popupDivPhone").html(responseMessage);
+			$(".popupDivPhone").show(600, function(){
+				timeoutID = window.setTimeout(hideDiv, 2000);
+				function hideDiv(){
+					$(".popupDivPhone").hide(600);
+				};
+			});
+		}else{
+			responseMessage = "Reservation Sent! We'll contact you shortly to confirm your stay at Dog Haüs"
+			console.log(responseMessage);
+			$(".popupDivSuccess").html(responseMessage);
+			$(".popupDivSuccess").show(600, function(){
+				timeoutID = window.setTimeout(hideDiv, 3000);
+				function hideDiv(){
+					$(".popupDivSuccess").hide(600);
+				};
+			});
 		}
 
 	});
